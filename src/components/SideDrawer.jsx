67c50ff2dfc110138faa323730556f6b75a20797
Shakti045 <shaktimandal045@gmail.com>
2024-04-02 23:38:08 +0530

    Update SideDrawer.jsx

--- src/components/SideDrawer.jsx
+++ src/components/SideDrawer.jsx
@@ -5,18 +5,20 @@ import { useSelector } from 'react-redux';
 import { ImCross } from "react-icons/im";
 
 
-const SideDrawer = ({open,setOpendrawer}) => {
+const SideDrawer = ({open,setOpendrawer,logout}) => {
   
 const {user} = useSelector((state)=>state.user);
 
   return (
     <div className={` absolute top-0 left-0 bottom-0  ${open && ' right-10 '} overflow-hidden bg-slate-300 p-4 `}>
-      <nav className=' flex flex-col gap-5'>
+      <nav className=' flex flex-col gap-5 '>
         <div className=' flex  justify-between'>
-            <div className=' flex flex-col gap-2 '>
-            <img src={user.avatarurl} alt='useravatar' className=' w-[40px] h-[40px] rounded-md'/>
-                <p className='  font-bold text-gray-600'>{user.username}</p>
-            </div>
+            {
+              user==null ? <Link to='/signup' className=' bg-pink-600 text-white px-3 py-2  rounded-md'>Sign Up</Link>:<div className=' flex gap-2 items-center'>
+                <img src={user.avatarurl} alt='useravatar' className='  w-[40px] h-[40px] rounded-md'/>
+                <p className=' text-gray-600'>{user.username}</p>
+              </div>
+            }
             <button className=' h-fit w-fit p-2 border-2 rounded-md border-slate-500'>
                 <ImCross onClick={()=>setOpendrawer(false)} className=' text-gray-600'/>
             </button>
@@ -37,6 +39,7 @@ const {user} = useSelector((state)=>state.user);
         <Link to='/profilecreate' >Update Profile</Link>
         </li>
        </ul>
+       <button onClick={logout} className='  bg-pink-600 text-white px-3 py-2  rounded-md'>Log out</button>
       </nav>
     </div>
   );
